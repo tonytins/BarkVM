842921c6141dbcc6caa2b9dabf4bd55b5d5c9dbd
Tony Bark <35226681+tonytins@users.noreply.github.com>
2026-08-06 13:52:06 -0400

    Brought back BarkVM enum and moved to Program.swift

--- Sources/BarkVM/Program.swift
+++ Sources/BarkVM/Program.swift
@@ -1,10 +1,44 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-//
-// Swift Argument Parser
-// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
-
 import ArgumentParser
+import Foundation
+
+public protocol OutputSink: AnyObject {
+    func write(_ line: String)
+}
+
+public final class CollectingOutput: OutputSink {
+    public private(set) var lines: [String] = []
+    
+    public init() { }
+    
+    public func write(_ line: String) {
+        lines.append(line)
+    }
+}
+
+public final class ConsoleOutput: OutputSink {
+    public init() { }
+    
+    public func write(_ line: String) {
+        print(line)
+    }
+}
+
+public protocol LanguageFrontend {
+    func parse(_ source: String) throws -> [Statement]
+}
+
+public enum BarkVM {
+    /// Compiles the source down to the JSON intermediate representation,
+    /* public static func compileToIR(
+        _ source: String,
+        using frontend: LanguageFrontend,
+        fancy: Bool = false
+    ) throws -> Data {
+        let statements = try frontend.parse(source)
+        let encoder = JSONEncoder()
+        return try encoder.encode(statements)
+    } */
+}
 
 @main
 struct Program: ParsableCommand {
